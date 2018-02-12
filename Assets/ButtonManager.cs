b4db11f2367d05c7c3a284a7bd80168b93a8e63a
Favian <fquach2@gmail.com>
2018-02-12 11:04:21 -0800

    Initial commit

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+     public void NewGameBtn(string newGameLevel)
+     {
+        SceneManager.LoadScene(newGameLevel);      
+     }
+
+
+    public void NewGameBtn()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Main Game/Control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    public float RotateSpeed = 30f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.forward * RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(-Vector3.forward * RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(Vector3.right * RotateSpeed * Time.deltaTime);
+         else if (Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(-Vector3.right * RotateSpeed * Time.deltaTime);
+    }
+}
